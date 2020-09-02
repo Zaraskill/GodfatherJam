37627afd9241e1686ae2f5431b0e68aa4c2e2eb2
CorentinLelong <corentin.lelong77@gmail.com>
2020-09-02 09:50:43 +0200

    Player Movement

---
+++ GodfatherSuperHero/Assets/Scripts/PlayerEntity.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : MonoBehaviour
+{
+
+    // Move
+    [Header("Move")]
+    public float acceleration = 20f;
+    public float moveSpeedMax = 10f;
+    private Vector2 moveDir;
+    private Vector2 speed = Vector2.zero;
+    private Vector2 orientDir = Vector2.right;
+
+    // Frictions
+    [Header("Friction")]
+    public float friction;
+    public float turnFriction;
+
+    //Rigidbody
+    [Header("Rigidbody")]
+    public Rigidbody2D _rigidbody;
+
+    // Debug
+    [Header("Debug")]
+    public bool _debugMode = false;
+
+
+    void Awake()
+    {
+        _rigidbody = GetComponentInChildren<Rigidbody2D>();
+        _rigidbody.gravityScale = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        UpdateMove();
+        UpdatePosition();
+    }
+
+    private void OnGUI()
+    {
+        if (!_debugMode)
+        {
+            return;
+        }
+
+        GUILayout.BeginVertical();
+        GUILayout.Label("Speed = " + speed);
+        GUILayout.Label("moveDir = " + moveDir);
+        GUILayout.EndVertical();
+    }
+
+
+    private void UpdatePosition()
+    {
+        _rigidbody.velocity = new Vector2(speed.x, speed.y);
+    }
+
+    #region Move
+
+    public void Move(Vector2 dir)
+    {
+        moveDir = dir;
+    }
+
+    private void UpdateMove()
+    {
+        if (moveDir != Vector2.zero)
+        {
+            float turnAngle = Vector2.SignedAngle(speed, moveDir);
+            turnAngle = Mathf.Abs(turnAngle);
+            float frictionRatio = turnAngle / 180f;
+            float turnFrictionWithRatio = turnFriction * frictionRatio;
+
+            speed += moveDir * acceleration * Time.fixedDeltaTime;
+            if (speed.sqrMagnitude > moveSpeedMax * moveSpeedMax)
+            {
+                speed = speed.normalized * moveSpeedMax;
+            }
+
+            Vector2 frictionDir = speed.normalized;
+            speed -= frictionDir * turnFrictionWithRatio * Time.fixedDeltaTime;
+
+            orientDir = speed.normalized;
+        }
+        else if (speed != Vector2.zero)
+        {
+            Vector2 frictionDir = speed.normalized;
+            float frictionToApply = friction * Time.fixedDeltaTime;
+            if (speed.sqrMagnitude <= frictionToApply * frictionToApply)
+            {
+                speed = Vector2.zero;
+            }
+            else
+            {
+                speed -= frictionToApply * frictionDir;
+            }
+        }
+    }
+
+    #endregion
+
+
+}
